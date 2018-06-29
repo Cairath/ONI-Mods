04a0b10eaa68bd4dbe2a076cf6c1d22f62ed53a4
Cairath <Cairath@users.noreply.github.com>
2018-06-29 23:56:42 +0200

    Fixed water sieve output mod

---
+++ src/WaterSieveFixedOutput/WaterSieveFixedOutput.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace WaterSieveFixedOutput
+{
+    public class WaterSieveFixedOutput
+    {
+	    [HarmonyPatch(typeof(WaterPurifierConfig), "ConfigureBuildingTemplate")]
+	    public static class WaterSieveFixedOutputPatch
+		{
+		    public static void Postfix(WaterPurifierConfig __instance, ref GameObject go)
+		    {
+				ElementConverter elementConverter = go.AddOrGet<ElementConverter>();
+
+			    var newOutputElements = new[]
+			    {
+				    new ElementConverter.OutputElement(5f, SimHashes.Water, 0f, true, 0f, 0.5f, true, 0.75f, 255, 0),
+				    new ElementConverter.OutputElement(0.2f, SimHashes.ToxicSand, 0f, true, 0f, 0.5f, true, 0.25f, 255, 0)
+			    };
+
+			    elementConverter.outputElements = newOutputElements;
+			}
+	    }
+	}
+}
