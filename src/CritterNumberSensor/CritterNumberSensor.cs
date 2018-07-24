640d76f6cb76d44ca0245274750de88a02819c0d
Cairath <Cairath@users.noreply.github.com>
2018-07-24 20:33:48 +0200

    Critter number sensor

---
+++ src/CritterNumberSensor/CritterNumberSensor.cs
@@ -0,0 +1,123 @@
+﻿using KSerialization;
+using STRINGS;
+using UnityEngine;
+
+namespace CritterNumberSensor
+{
+	[SerializationConfig(MemberSerialization.OptIn)]
+	public class CritterNumberSensor : Switch, ISaveLoadable, IThresholdSwitch, ISim200ms
+	{
+		private static readonly HashedString[] ON_ANIMS = new HashedString[2] { "on_pre", "on_loop" };
+		private static readonly HashedString[] OFF_ANIMS = new HashedString[2] { "on_pst", "off" };
+
+		[SerializeField]
+		[Serialize]
+		private float threshold;
+		[SerializeField]
+		[Serialize]
+		private bool activateAboveThreshold = true;
+		private bool wasOn;
+		private KBatchedAnimController animController;
+		private int currentCritters = 0;
+
+		public float CurrentValue => currentCritters;
+		public LocString Title => "Critter Number Sensor";
+		public LocString ThresholdValueName => UI.CODEX.CATEGORYNAMES.CREATURES;
+		public LocString ThresholdValueUnits() => "";
+		public string AboveToolTip => "Sensor will be on if the number of critters is above {0}";
+		public string BelowToolTip => "Sensor will be on if the number of critters is below {0}";
+		public float RangeMin => 0.0f;
+		public float RangeMax => 50.0f;
+		public float GetRangeMinInputField() => 0.0f;
+		public float GetRangeMaxInputField() => 50.0f;
+
+		protected override void OnSpawn()
+		{
+			base.OnSpawn();
+			animController = GetComponent<KBatchedAnimController>();
+			OnToggle += OnSwitchToggled;
+			UpdateLogicCircuit();
+			UpdateVisualState(true);
+			wasOn = IsSwitchedOn;
+		}
+
+		public void Sim200ms(float dt)
+		{
+			currentCritters = Game.Instance.roomProber.GetCavityForCell(Grid.PosToCell(this)).creatures.Count;
+
+			if (activateAboveThreshold)
+			{
+				if (currentCritters > threshold && !IsSwitchedOn)
+				{
+					Toggle();
+				}
+				else if (currentCritters <= threshold && IsSwitchedOn)
+				{
+					Toggle();
+				}
+			}
+			else if (!activateAboveThreshold)
+			{
+				if (currentCritters < threshold && !IsSwitchedOn)
+				{
+					Toggle();
+				}
+				else if (currentCritters >= threshold && IsSwitchedOn)
+				{
+					Toggle();
+				}
+			}
+		}
+
+		private void OnSwitchToggled(bool toggled_on)
+		{
+			UpdateLogicCircuit();
+			UpdateVisualState();
+		}
+
+		private void UpdateVisualState(bool force = false)
+		{
+			if (wasOn == switchedOn && !force)
+				return;
+
+			wasOn = switchedOn;
+
+			if (switchedOn)
+				animController.Play(ON_ANIMS, KAnim.PlayMode.Loop);	
+			else
+				animController.Play(OFF_ANIMS, KAnim.PlayMode.Once);
+		}
+
+		public float Threshold
+		{
+			get => threshold;
+			set => threshold = value;
+		}
+
+		public bool ActivateAboveThreshold
+		{
+			get => activateAboveThreshold;
+			set => activateAboveThreshold = value;
+		}
+
+		private void UpdateLogicCircuit()
+		{
+			GetComponent<LogicPorts>().SendSignal(LogicSwitch.PORT_ID, !switchedOn ? 0 : 1);
+		}	
+
+		public string Format(float value, bool units)
+		{
+			return GameUtil.GetFormattedInt(Mathf.RoundToInt(value));
+		}
+
+		public float ProcessedSliderValue(float input)
+		{
+			return Mathf.RoundToInt(input);
+		}
+
+		public float ProcessedInputValue(float input)
+		{
+			return Mathf.RoundToInt(input);
+		}
+	}
+}
---
+++ src/CritterNumberSensor/CritterNumberSensorConfig.cs
@@ -0,0 +1,66 @@
+﻿using OverlayModes;
+using TUNING;
+using UnityEngine;
+
+namespace CritterNumberSensor
+{
+	public class CritterNumberSensorConfig : IBuildingConfig
+	{
+		public static string ID = "LogicCritterNumberSensor";
+
+		public override BuildingDef CreateBuildingDef()
+		{
+			string id = CritterNumberSensorConfig.ID;
+			int width = 1;
+			int height = 1;
+			string anim = "diseasesensor_kanim";
+			int hitpoints = 30;
+			float construction_time = 30f;
+			float[] construction_mass = new float[2]
+			{
+				BUILDINGS.CONSTRUCTION_MASS_KG.TIER0[0],
+				BUILDINGS.CONSTRUCTION_MASS_KG.TIER1[0]
+			};
+			string[] construction_materials = new string[2]
+			{
+				"RefinedMetal",
+				"Plastic"
+			};
+			float melting_point = 1600f;
+			BuildLocationRule build_location_rule = BuildLocationRule.Anywhere;
+			EffectorValues none = NOISE_POLLUTION.NONE;
+			BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(id, width, height, anim, hitpoints, construction_time, construction_mass, construction_materials, melting_point, build_location_rule, BUILDINGS.DECOR.PENALTY.TIER0, none, 0.2f);
+			buildingDef.Overheatable = false;
+			buildingDef.Floodable = false;
+			buildingDef.Entombable = false;
+			buildingDef.ViewMode = SimViewMode.Logic;
+			buildingDef.AudioCategory = "Metal";
+			buildingDef.SceneLayer = Grid.SceneLayer.Building;
+			SoundEventVolumeCache.instance.AddVolume("diseasesensor_kanim", "PowerSwitch_on", NOISE_POLLUTION.NOISY.TIER3);
+			SoundEventVolumeCache.instance.AddVolume("diseasesensor_kanim", "PowerSwitch_off", NOISE_POLLUTION.NOISY.TIER3);
+			GeneratedBuildings.RegisterWithOverlay(Logic.HighlightItemIDs, CritterNumberSensorConfig.ID);
+
+			return buildingDef;
+		}
+
+		public override void DoPostConfigurePreview(BuildingDef def, GameObject go)
+		{
+			GeneratedBuildings.RegisterLogicPorts(go, LogicSwitchConfig.OUTPUT_PORT);
+		}
+
+		public override void DoPostConfigureUnderConstruction(GameObject go)
+		{
+			GeneratedBuildings.RegisterLogicPorts(go, LogicSwitchConfig.OUTPUT_PORT);
+		}
+
+		public override void DoPostConfigureComplete(GameObject go)
+		{
+			GeneratedBuildings.MakeBuildingAlwaysOperational(go);
+			GeneratedBuildings.RegisterLogicPorts(go, LogicSwitchConfig.OUTPUT_PORT);
+			CritterNumberSensor critterNumberSensor = go.AddOrGet<CritterNumberSensor>();
+			critterNumberSensor.Threshold = 0.0f;
+			critterNumberSensor.ActivateAboveThreshold = true;
+			critterNumberSensor.manuallyControlled = false;
+		}
+	}
+}
