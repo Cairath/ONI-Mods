37ce59db93826c3863c3c2e187c442ad6b0889dc
Cairath <Cairath@users.noreply.github.com>
2018-07-29 17:44:13 +0200

    Prettier big wire

---
+++ src/PrettierConductiveHeavyWattWire/PrettierConductiveHeavyWattWireMod.cs
@@ -0,0 +1,17 @@
+﻿using Harmony;
+
+namespace PrettierConductiveHeavyWattWire
+{
+    public class PrettierConductiveHeavyWattWireMod
+    {
+	    [HarmonyPatch(typeof(WireRefinedHighWattageConfig), "CreateBuildingDef")]
+	    public static class PrettierConductiveHeavyWattWirePatch
+		{
+		    public static void Postfix(ref BuildingDef __result)
+		    {
+			    __result.BaseDecor = -5f;
+			    __result.BaseDecorRadius = 3;
+		    }
+	    }
+	}
+}
