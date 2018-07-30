7c705d8ac820542273ff408b59675c9342c5f088
Cairath <Cairath@users.noreply.github.com>
2018-07-30 14:39:14 +0200

    mod to set polluted water heat capacity to the same as clean water

--- src/DrywallNotEntombed/DrywallNotEntombedMod.cs
+++ src/DrywallNotEntombed/DrywallNotEntombedMod.cs
@@ -1,16 +1,22 @@
-﻿using Harmony;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
 
 namespace DrywallNotEntombed
 {
-    public class DrywallNotEntombedMod
-    {
-	    [HarmonyPatch(typeof(ExteriorWallConfig), "CreateBuildingDef")]
-	    public static class DrywallNotEntombedPatch
+	public class DrywallNotEntombedMod
+	{
+		[HarmonyPatch(typeof(ExteriorWallConfig), "CreateBuildingDef")]
+		public static class DrywallNotEntombedPatch
 		{
-		    public static void Postfix(ref BuildingDef __result)
-		    {
-			    __result.Entombable = false;
-		    }
-	    }
+			public static void Postfix(ref BuildingDef __result)
+			{
+				__result.Entombable = false;
+			}
+		}
 	}
 }
