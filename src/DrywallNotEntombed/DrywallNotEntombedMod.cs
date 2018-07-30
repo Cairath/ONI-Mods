12138c2fa325b86ee63a988739f31bcfbcc0bd1a
Cairath <Cairath@users.noreply.github.com>
2018-07-30 11:59:22 +0200

    Drywall no longer entombed

---
+++ src/DrywallNotEntombed/DrywallNotEntombedMod.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+
+namespace DrywallNotEntombed
+{
+    public class DrywallNotEntombedMod
+    {
+	    [HarmonyPatch(typeof(ExteriorWallConfig), "CreateBuildingDef")]
+	    public static class DrywallNotEntombedPatch
+		{
+		    public static void Postfix(ref BuildingDef __result)
+		    {
+			    __result.Entombable = false;
+		    }
+	    }
+	}
+}
