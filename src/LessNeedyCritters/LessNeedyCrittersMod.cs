13326e74e37f1c42983891cd0460312335708b30
Cairath <Cairath@users.noreply.github.com>
2018-09-11 18:30:16 +0200

    Less Needy Critters

---
+++ src/LessNeedyCritters/LessNeedyCrittersMod.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+
+namespace LessNeedyCritters
+{
+    public class LessNeedyCrittersMod
+    {
+	    [HarmonyPatch(typeof(ModifierSet), "LoadEffects")]
+	    public class ModifierSetPatch
+		{
+		    private static void Postfix(ref ModifierSet __instance)
+		    {
+			    __instance.effects.Get("Ranched").duration = 1800f;
+		    }
+	    }
+	}
+}
