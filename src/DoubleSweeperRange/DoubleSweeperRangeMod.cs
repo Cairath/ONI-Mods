da35fd03d4bef2c4bc00067dbf61225d0951342d
Cairath <Cairath@users.noreply.github.com>
2018-09-11 20:07:31 +0200

    Double sweeper range. Does not display correct range under construction, otherwise ok.

---
+++ src/DoubleSweeperRange/DoubleSweeperRangeMod.cs
@@ -0,0 +1,27 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace DoubleSweeperRange
+{
+    public class DoubleSweeperRangeMod
+    {
+	    [HarmonyPatch(typeof(SolidTransferArmConfig), "DoPostConfigurePreview")]
+	    public class DoPostConfigurePreviewPatch
+		{
+		    private static void Postfix(ref GameObject go)
+		    {
+			    go.GetComponent<StationaryChoreRangeVisualizer>().range = 8;
+			}
+	    }
+
+		[HarmonyPatch(typeof(SolidTransferArmConfig), "DoPostConfigureComplete")]
+	    public class DoPostConfigureCompletePatch
+		{
+		    private static void Postfix(ref GameObject go)
+		    {
+			    go.GetComponent<StationaryChoreRangeVisualizer>().range = 8;
+			    go.GetComponent<SolidTransferArm>().pickupRange = 8;
+			}
+	    }
+	}
+}
