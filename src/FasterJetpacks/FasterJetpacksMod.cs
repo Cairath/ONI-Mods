4b03027463da0b0b5f7d85cc575b0c5d75829d94
Cairath <Cairath@users.noreply.github.com>
2018-10-12 12:30:16 +0200

    Faster basic jetpacks (x3)

---
+++ src/FasterJetpacks/FasterJetpacksMod.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+
+namespace FasterJetpacks
+{
+    public class FasterJetpacksMod
+    {
+	    [HarmonyPatch(typeof(BipedTransitionLayer))]
+		[HarmonyPatch(new [] { typeof(Navigator), typeof(float), typeof(float) })]
+	    public static class BipedTransitionLayerPatch
+		{
+		    public static void Postfix(ref BipedTransitionLayer __instance)
+		    {
+			    var instance = Traverse.Create(__instance);
+
+				var jetpackSpeed = instance.Field("jetPackSpeed").GetValue<float>();
+			    instance.Field("jetPackSpeed").SetValue(jetpackSpeed * 3f);
+		    }
+	    }
+	}
+}
