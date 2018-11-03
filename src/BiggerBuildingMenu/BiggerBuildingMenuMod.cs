b131aa14c42ec69db8fc020b61ddc7d07a744667
Cairath <Cairath@users.noreply.github.com>
2018-11-03 23:35:47 +0100

    Bigger Building Menu

---
+++ src/BiggerBuildingMenu/BiggerBuildingMenuMod.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+
+namespace BiggerBuildingMenu
+{
+    public class BiggerBuildingMenuMod
+    {
+	    [HarmonyPatch(typeof(PlanScreen), MethodType.Constructor)]
+	    public static class PlanScreenPatch
+		{
+		    public static void Postfix(PlanScreen __instance)
+		    {
+			    var ps = Traverse.Create(__instance).Field("buildGrid_maxRowsBeforeScroll").SetValue(6);
+		    }
+	    }
+	}
+}
