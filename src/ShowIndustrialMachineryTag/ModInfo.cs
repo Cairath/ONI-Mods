879b718598147228fb02bac36237a032bd4bb80a
Cairath <Cairath@users.noreply.github.com>
2019-02-11 22:53:28 +0100

    ShowIndustrialMachineryTag - shows whether the building is industrial *after* it is built

---
+++ src/ShowIndustrialMachineryTag/ModInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace ShowIndustrialMachineryTag
+{
+	public static class ModInfo
+	{
+		public static string Name = "Show Industrial Machinery Tag";
+		public static int Version = 1;
+	}
+}
---
+++ src/ShowIndustrialMachineryTag/Patches.cs
@@ -0,0 +1,47 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace ShowIndustrialMachineryTag
+{
+	public static class Patches
+	{
+		[HarmonyPatch(typeof(SplashMessageScreen))]
+		[HarmonyPatch("OnPrefabInit")]
+		public static class SplashMessageScreen_OnPrefabInit_Patch
+		{
+			public static void Postfix()
+			{
+				CaiLib.ModCounter.ModCounter.Hit(ModInfo.Name, ModInfo.Version);
+				CaiLib.Logger.LogInit(ModInfo.Name, ModInfo.Version);
+			}
+		}
+
+		[HarmonyPatch(typeof(ProductInfoScreen))]
+		[HarmonyPatch("SetDescription")]
+		public static class ProductInfoScreen_SetDescription_Patch
+		{
+			public static void Postfix(ref ProductInfoScreen __instance, BuildingDef def)
+			{
+				__instance.productFlavourText.text += "\n\n<b>This is a test</b>";
+			}
+		}
+
+		[HarmonyPatch(typeof(SimpleInfoScreen))]
+		[HarmonyPatch("SetPanels")]
+		public static class SimpleInfoScreen_SetPanels_Patch
+		{
+			public static void Postfix(ref SimpleInfoScreen __instance, GameObject target)
+			{
+				if (target.GetComponent<KPrefabID>().HasTag(RoomConstraints.ConstraintTags.IndustrialMachinery))
+				{
+					var field = Traverse.Create(__instance).Field("descriptionContainer");
+					var descriptionContainer = field.GetValue<DescriptionContainer>();
+					descriptionContainer.flavour.text += "\n\n<color=\"red\"><b>This is an industrial machine.</b></color>";
+
+					field.SetValue(descriptionContainer);
+				}
+			}
+		}
+
+	}
+}
