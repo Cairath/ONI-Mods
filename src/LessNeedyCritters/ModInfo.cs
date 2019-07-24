d859e59a9e2fe692a2c57d3f60e427014d99034d
Cairath <Cairath@users.noreply.github.com>
2019-07-24 22:12:37 +0200

    Another one bites the dust

--- src/LessNeedyCritters/ModInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LessNeedyCritters
-{
-	public static class ModInfo
-	{
-		public static string Name = "Less Needy Critters";
-		public static string Version = System.DateTime.UtcNow.ToString("yyyy.MM.dd");
-	}
-}
