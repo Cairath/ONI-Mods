c648685d2290ffdf087209addec70103b85a944a
Cairath <Cairath@users.noreply.github.com>
2019-07-26 05:04:21 +0200

    Updated RefinedMetalsUsableAsRawMetals

--- src/RefinedMetalsUsableAsRawMetals/RefinedMetalsUsableAsRawMetalsPatches.cs
+++ src/RefinedMetalsUsableAsRawMetals/RefinedMetalsUsableAsRawMetalsPatches.cs
@@ -4,63 +4,42 @@ using static CaiLib.Logger.Logger;
 
 namespace RefinedMetalsUsableAsRawMetals
 {
-	public static class RefinedMetalsUsableAsRawMetalsPatches
-	{
-		[HarmonyPatch(typeof(SplashMessageScreen))]
-		[HarmonyPatch("OnPrefabInit")]
-		public static class SplashMessageScreen_OnPrefabInit_Patch
-		{
-			public static void Postfix()
-			{
-				LogInit(ModInfo.Name, ModInfo.Version);
-			}
-		}
+    public static class RefinedMetalsUsableAsRawMetalsPatches
+    {
+        [HarmonyPatch(typeof(SplashMessageScreen))]
+        [HarmonyPatch("OnPrefabInit")]
+        public static class SplashMessageScreen_OnPrefabInit_Patch
+        {
+            public static void Postfix()
+            {
+                LogInit(ModInfo.Name, ModInfo.Version);
+            }
+        }
 
-		[HarmonyPatch(typeof(ElementLoader))]
-		[HarmonyPatch("FinaliseElementsTable")]
-		public static class ElementLoader_LoadUserElementData_Patch
-		{
-			public static void Postfix()
-			{
-				var copper = ElementLoader.FindElementByHash(SimHashes.Copper);
-				var iron = ElementLoader.FindElementByHash(SimHashes.Iron);
-				var tungsten = ElementLoader.FindElementByHash(SimHashes.Tungsten);
-				var gold = ElementLoader.FindElementByHash(SimHashes.Gold);
-				var lead = ElementLoader.FindElementByHash(SimHashes.Lead);
-				var aluminum = ElementLoader.FindElementByHash(SimHashes.Aluminum);
+        [HarmonyPatch(typeof(ElementLoader))]
+        [HarmonyPatch("FinaliseElementsTable")]
+        public static class ElementLoader_LoadUserElementData_Patch
+        {
+            public static void Postfix()
+            {
+                var copper = ElementLoader.FindElementByHash(SimHashes.Copper);
+                var iron = ElementLoader.FindElementByHash(SimHashes.Iron);
+                var tungsten = ElementLoader.FindElementByHash(SimHashes.Tungsten);
+                var gold = ElementLoader.FindElementByHash(SimHashes.Gold);
+                var lead = ElementLoader.FindElementByHash(SimHashes.Lead);
+                var aluminum = ElementLoader.FindElementByHash(SimHashes.Aluminum);
 
-				var basic = new[] { copper, iron, gold, lead, aluminum };
+                var elements = new[] { copper, iron, gold, lead, aluminum, tungsten };
 
-				foreach (var element in basic)
-				{
-					element.oreTags = CreateTags(element.materialCategory, new[] { "Burns", "BuildableAny", "RefinedMetal", "Metal" });
-					GameTags.SolidElements.Add(element.tag);
-				}
+                foreach (var element in elements)
+                {
+                    var tags = new List<Tag>(element.oreTags) { GameTags.Metal };
+                    element.oreTags = tags.ToArray();
 
-				tungsten.oreTags =
-					CreateTags(tungsten.materialCategory, new[] { "Plumbable", "BuildableAny", "RefinedMetal", "Metal" });
-				GameTags.SolidElements.Add(tungsten.tag);
-			}
-		}
+                    GameTags.SolidElements.Add(element.tag);
+                }
 
-		private static Tag[] CreateTags(Tag materialCategory, string[] tags)
-		{
-			var tagList = new List<Tag>();
-			if (tags != null)
-			{
-				foreach (var tagString in tags)
-				{
-					if (!string.IsNullOrEmpty(tagString))
-						tagList.Add(TagManager.Create(tagString));
-				}
-			}
-
-			tagList.Add(TagManager.Create(Element.State.Solid.ToString()));
-
-			if (materialCategory.IsValid && !tagList.Contains(materialCategory))
-				tagList.Add(materialCategory);
-
-			return tagList.ToArray();
-		}
-	}
+            }
+        }
+    }
 }
