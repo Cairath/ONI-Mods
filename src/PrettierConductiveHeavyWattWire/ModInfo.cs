9ea7114591c686d499e6eaa803b4abf10edfa32a
Cairath <Cairath@users.noreply.github.com>
2019-07-27 17:40:22 +0200

    Deprecated Prettier Conductive Heavy Wire

--- src/PrettierConductiveHeavyWattWire/ModInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PrettierConductiveHeavyWattWire
-{
-	public static class ModInfo
-	{
-		public static string Name = "Prettier Conductive Heavy-Watt Wire";
-		public static string Version = System.DateTime.UtcNow.ToString("yyyy.MM.dd");
-	}
-}
