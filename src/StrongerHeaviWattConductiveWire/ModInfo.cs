686190b236613306042f6c5a15e60b848df7b6bc
Cairath <Cairath@users.noreply.github.com>
2019-07-27 17:40:44 +0200

    Added Stronger Heavi-Watt Conductive Wire (50k)

---
+++ src/StrongerHeaviWattConductiveWire/ModInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace StrongerHeaviWattConductiveWire
+{
+	public static class ModInfo
+	{
+		public static string Name = "StrongerHeaviWattConductiveWire";
+		public static string Version = System.DateTime.UtcNow.ToString("yyyy.MM.dd");
+	}
+}
