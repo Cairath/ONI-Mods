95473792610bf08d1d21b242c0b757918c193af8
Cairath <Cairath@users.noreply.github.com>
2019-07-27 04:22:49 +0200

    Klei murdered the splash screen :(

--- src/BiggerBuildingMenu/BiggerBuildingMenuPatches.cs
+++ src/BiggerBuildingMenu/BiggerBuildingMenuPatches.cs
@@ -9,11 +9,9 @@ namespace BiggerBuildingMenu
 	{
 		private static ConfigManager<Config> _configManager;
 
-		[HarmonyPatch(typeof(SplashMessageScreen))]
-		[HarmonyPatch("OnPrefabInit")]
-		public static class SplashMessageScreen_OnPrefabInit_Patch
+		public static class BiggerBuildingMenu_OnLoad
 		{
-			public static void Postfix()
+			public static void OnLoad()
 			{
 				LogInit(ModInfo.Name, ModInfo.Version);
 				_configManager = new ConfigManager<Config>(ModInfo.Name, Assembly.GetExecutingAssembly().Location);
