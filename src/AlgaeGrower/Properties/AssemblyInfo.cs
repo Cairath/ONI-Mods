7e1372bd7bb85313353c4ed76f000679a63a02f9
Cairath <Cairath@users.noreply.github.com>
2019-09-05 03:36:30 +0200

    Algae Grower - added preview

--- src/AlgaeGrower/Properties/AssemblyInfo.cs
+++ src/AlgaeGrower/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.3.5.1")]
-[assembly: AssemblyFileVersion("2019.9.5.1")]
+[assembly: AssemblyVersion("2019.3.5.4")]
+[assembly: AssemblyFileVersion("2019.9.5.4")]
 
