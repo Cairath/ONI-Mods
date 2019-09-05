08ff669ba194fd728d4c8563a6ebe76baf61ffe0
Cairath <Cairath@users.noreply.github.com>
2019-09-05 03:35:41 +0200

    Removed 50kW heavi wire

--- src/StrongerHeaviWattConductiveWire/ModInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StrongerHeaviWattConductiveWire
-{
-	public static class ModInfo
-	{
-		public static string Name = "StrongerHeaviWattConductiveWire";
-		public static string Version = System.DateTime.UtcNow.ToString("yyyy.MM.dd");
-	}
-}
