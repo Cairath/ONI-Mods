8f79054ed84d0ddd81e13dd6897add8f7a753fbf
Cairath <Cairath@users.noreply.github.com>
2020-02-20 10:56:56 +0100

    Updated all mods to .NET Framework 4.0

--- src/CaiLib/Properties/AssemblyInfo.cs
+++ src/CaiLib/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.1.19.31")]
-[assembly: AssemblyFileVersion("2020.1.19.29")]
+[assembly: AssemblyVersion("2020.1.24.31")]
+[assembly: AssemblyFileVersion("2020.1.24.29")]
