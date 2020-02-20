8bf9693a24f86721b0a327aa048ac74dc6394c5b
Cairath <Cairath@users.noreply.github.com>
2020-02-20 11:35:58 +0100

    Edited post build to include template instead of config itself

--- src/BiggerBuildingMenu/Properties/AssemblyInfo.cs
+++ src/BiggerBuildingMenu/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2020.2.20.0")]
-[assembly: AssemblyFileVersion("2020.2.20.6")]
+[assembly: AssemblyFileVersion("2020.2.20.7")]
