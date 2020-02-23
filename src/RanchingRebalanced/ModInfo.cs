92ddea9a04a05717b748f8f42782b286c3609304
Cairath <Cairath@users.noreply.github.com>
2020-02-23 04:13:34 +0100

    Removed Ranching Rebalanced from master for now

--- src/RanchingRebalanced/ModInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RanchingRebalanced
-{
-	public static class ModInfo
-	{
-		public static string Name = "Ranching Rebalanced";
-		public static string Version = System.DateTime.UtcNow.ToString("yyyy.MM.dd");
-	}
-}
