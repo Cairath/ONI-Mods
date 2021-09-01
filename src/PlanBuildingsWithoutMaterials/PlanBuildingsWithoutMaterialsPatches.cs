5773a98dd03e268c6f478ed2ee8dab33e93955dc
G'lek Tarssza <glek@glektarssza.com>
2021-09-01 18:01:47 -0400

    Fixed "Plan Buildings without Materials".

--- src/PlanBuildingsWithoutMaterials/PlanBuildingsWithoutMaterialsPatches.cs
+++ src/PlanBuildingsWithoutMaterials/PlanBuildingsWithoutMaterialsPatches.cs
@@ -2,17 +2,31 @@
 
 namespace PlanBuildingsWithoutMaterials
 {
-	public class PlanBuildingsWithoutMaterialsPatches
-	{
-		[HarmonyPatch(typeof(MaterialSelector))]
-		[HarmonyPatch(nameof(MaterialSelector.AllowInsufficientMaterialBuild))]
-		public static class MaterialSelector_AllowInsufficientMaterialBuild_Patch
-		{
-			public static bool Prefix(ref bool __result)
-			{
-				__result = true;
-				return false;
-			}
-		}
-	}
+    [HarmonyPatch]
+    public class PlanBuildingsWithoutMaterialsPatches
+    {
+        [HarmonyPatch(typeof(MaterialSelector))]
+        [HarmonyPatch("AllowInsufficientMaterialBuild")]
+        public static class MaterialSelector_AllowInsufficientMaterialBuild_Patch
+        {
+            public static bool Prefix(ref bool __result)
+            {
+                __result = true;
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(PlanScreen))]
+        [HarmonyPatch("GetBuildableStateForDef")]
+        public static class PlanScreen_GetBuildableStateForDef_Patch
+        {
+            public static void Postfix(ref PlanScreen.RequirementsState __result)
+            {
+                if (__result == PlanScreen.RequirementsState.Materials)
+                {
+                    __result = PlanScreen.RequirementsState.Complete;
+                }
+            }
+        }
+    }
 }
